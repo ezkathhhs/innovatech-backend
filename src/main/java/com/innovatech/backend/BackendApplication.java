97d567390d4709bf4ca7de9b6044cb4b035dad2d
ezkathhhs <matiazloyola@gmail.com>
2026-07-16 21:59:37 -0400

    Fuerza despliegue a ECR

--- src/main/java/com/innovatech/backend/BackendApplication.java
+++ src/main/java/com/innovatech/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.innovatech.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class BackendApplication {
 
